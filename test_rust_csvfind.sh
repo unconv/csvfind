5f971d067a11e5d2cc3701d4de3d58ac57ba6adc
yassinouk <118069870+yassinouk@users.noreply.github.com>
2023-05-22 19:18:52 +0100

    Update test_rust_csvfind.sh
    adjusting the code, and fixing some typo mistakes.

--- test_rust_csvfind.sh
+++ test_rust_csvfind.sh
@@ -3,7 +3,7 @@
 # Compile csvfind.rs
 rustc -O -o rust-csvfind csvfind.rs
 
-# Compile csvfind.rs
+# Compile csvfind_v2.rs
 rustc -O -o rust-csvfind_v2 csvfind_v2.rs
 
 # Wait for compilation to complete (adjust the duration as needed)
@@ -19,11 +19,12 @@ if cmp -s expected_output.txt output1.txt; then
   rm -rf output1.txt
 else
   echo "Test failed: Output does not match expected result for csvfind.rs "
+  cat output1.txt
 fi
 
 # Test csvfind_v2.rs
 echo "Testing csvfind_v2.rs"
-./rust-csvfind products.csv sunglasses name > output2.txt
+./rust-csvfind_v2 products.csv sunglasses name > output2.txt
 
 # Compare output2 with expected file
 if cmp -s expected_output.txt output2.txt; then
@@ -31,4 +32,5 @@ if cmp -s expected_output.txt output2.txt; then
   rm -rf output2.txt
 else
   echo "Test failed: Output does not match expected result for csvfind_v2.rs "
+  cat output2.txt
 fi
