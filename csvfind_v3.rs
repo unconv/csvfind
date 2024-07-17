f14e9590741e00cb3d74013809ae78dbb3bdbc8f
beeb <703631+beeb@users.noreply.github.com>
2024-07-17 13:56:17 +0200

    feat(rust): case insensitive column filter

--- csvfind_v3.rs
+++ csvfind_v3.rs
@@ -10,7 +10,7 @@ fn main() -> std::io::Result<()> {
     args.next(); // Skip the program name
     let input_file_path = args.next().expect(USAGE);
     let search_term = args.next().map(|s| s.to_ascii_lowercase()).expect(USAGE);
-    let header_name = args.next().map(|col| col.trim().to_string());
+    let header_name = args.next().map(|col| col.trim().to_ascii_lowercase());
 
     let input_file = File::open(input_file_path)?;
 
@@ -23,9 +23,12 @@ fn main() -> std::io::Result<()> {
 
     println!("{}", raw_headers);
 
-    let header_idx = header_name
-        .as_ref()
-        .and_then(|col| raw_headers.split(',').map(str::trim).position(|h| h == col));
+    let header_idx = header_name.as_ref().and_then(|filter_col| {
+        raw_headers
+            .split(',')
+            .map(|col| col.trim().to_ascii_lowercase())
+            .position(|col| &col == filter_col)
+    });
 
     for line in lines.map_while(Result::ok) {
         if !line.to_ascii_lowercase().contains(&search_term) {
